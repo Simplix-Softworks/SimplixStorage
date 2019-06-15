8e2b98f0cab8538177d05b8632f734f3a4f7b254
leonhard <JavaFactoryDev@gmail.com>
2019-06-15 13:25:53 +0200

    Put .github to gitignore

--- src/main/java/de/leonhard/storage/Main.java
+++ src/main/java/de/leonhard/storage/Main.java
@@ -2,18 +2,16 @@ package de.leonhard.storage;
 
 
 class Main {
-//    /*
-//    https://www.google.com/search?client=ubuntu&channel=fs&q=java+merge+two+files&ie=utf-8&oe=utf-8
-//    https://github.com/TheElectronWill/TOML-javalib
-//
-//    Merge Files:
-//    https://stackoverflow.com/questions/14673063/merging-multiple-files-in-java
-//
-//     */
-//
-//    public static void main(String[] args) throws IOException, InterruptedException {
-//
-//    }
+    final static String DATADIR = "";
 
+
+    public static void main(String[] args) {
+
+
+        Yaml yaml = new Yaml("comments", "");
+        yaml.setConfigSettings(ConfigSettings.preserveComments);
+        yaml.set("a", 34);
+
+    }
 
 }
