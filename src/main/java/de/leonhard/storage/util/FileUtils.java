39251450371d3d98d651746dade1feb425ba7c0f
leonhard <JavaFactoryDev@gmail.com>
2019-06-24 21:03:47 +0200

    Updates FileUtils

--- src/main/java/de/leonhard/storage/util/FileUtils.java
+++ src/main/java/de/leonhard/storage/util/FileUtils.java
@@ -1,6 +1,11 @@
 package de.leonhard.storage.util;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileUtils {
 
@@ -15,6 +20,12 @@ public class FileUtils {
 
     public static boolean hasNotChanged(final File file, final long timeStamp) {
         return file.lastModified() <= timeStamp;
+    }
+
+    public static List<String> readAllLines(final File file) throws IOException {
+        final byte[] fileBytes = Files.readAllBytes(file.toPath());
+        final String asString = new String(fileBytes);
+        return new ArrayList<>(Arrays.asList(asString.split("\n")));
     }
 
     public static void deleteFile(String filename, String path) {
