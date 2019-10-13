e7226e9d2e532f0796e0a064822afb9556891b73
Zeanon <thezeanon@gmail.com>
2019-10-13 18:23:00 +0200

    Update

--- src/main/java/de/leonhard/storage/util/FileUtils.java
+++ src/main/java/de/leonhard/storage/util/FileUtils.java
@@ -7,35 +7,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@SuppressWarnings({"unused", "WeakerAccess", "ResultOfMethodCallIgnored"})
+@SuppressWarnings({"unused"})
 public class FileUtils {
-
-    public static File getFile(String filename, String path) {
-        return new File(path, filename);
-    }
-
-    public static boolean exists(String filename, String path) {
-        return getFile(filename, path).exists();
-    }
 
     public static boolean hasNotChanged(final File file, final long timeStamp) {
         return file.lastModified() <= timeStamp;
     }
 
-    public static List<String> readAllLines(final File file) throws IOException {
-        return getStrings(file);
-    }
-
-    public static List<String> getStrings(File file) throws IOException {
+    private static List<String> readAllLines(File file) throws IOException {
         final byte[] fileBytes = Files.readAllBytes(file.toPath());
         final String asString = new String(fileBytes);
         return new ArrayList<>(Arrays.asList(asString.split("\n")));
-    }
-
-    public static void deleteFile(String filename, String path) {
-        final File file = new File(path, filename);
-        if (file.exists()) {
-            file.delete();
-        }
     }
 }
