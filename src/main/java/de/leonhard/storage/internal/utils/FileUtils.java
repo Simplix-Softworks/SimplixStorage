7728e1e8f7ba4ab6fba304e77e7748dbc6f9ed9f
Zeanon <thezeanon@gmail.com>
2019-10-15 20:18:20 +0200

    Fixed Version

--- src/main/java/de/leonhard/storage/internal/utils/FileUtils.java
+++ src/main/java/de/leonhard/storage/internal/utils/FileUtils.java
@@ -1,67 +1,75 @@
 package de.leonhard.storage.internal.utils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileUtils {
 
-	public static boolean hasChanged(final File file, final long timeStamp) {
-		return timeStamp < file.lastModified();
-	}
+    public static boolean hasChanged(final File file, final long timeStamp) {
+        return timeStamp < file.lastModified();
+    }
 
-	public static File getAndMake(final String name, final String path) {
-		return getAndMake(new File(path, name));
-	}
+    @SuppressWarnings("unused")
+    public static File getAndMake(final String name, final String path) {
+        return getAndMake(new File(path, name));
+    }
 
-	public static InputStream createNewInputStream(final File file) {
-		try {
-			return Files.newInputStream(file.toPath());
-		} catch (IOException e) {
-			System.err.println("Exception while creating InputStream from '" + file.getName() + "'");
-			System.err.println("At: '" + file.getAbsolutePath() + "'");
-			e.printStackTrace();
-			throw new IllegalStateException("InputStream would be null");
-		}
-	}
+    public static InputStream createNewInputStream(final File file) {
+        try {
+            return Files.newInputStream(file.toPath());
+        } catch (IOException e) {
+            System.err.println("Exception while creating InputStream from '" + file.getName() + "'");
+            System.err.println("At: '" + file.getAbsolutePath() + "'");
+            e.printStackTrace();
+            throw new IllegalStateException("InputStream would be null");
+        }
+    }
 
 
-	public static void copy(final InputStream inputStream, final Path destination) {
-		try {
-			Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
-		} catch (final Exception exception) {
-			System.err.println("Exception while copying to + '" + destination.toAbsolutePath() + "'");
-			exception.printStackTrace();
-		}
-	}
+    public static void writeToFile(final File file, final InputStream inputStream) {
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            if (!file.exists()) {
+                Files.copy(inputStream, file.toPath());
+            } else {
+                final byte[] data = new byte[8192];
+                int count;
+                while ((count = inputStream.read(data, 0, 8192)) != -1) {
+                    outputStream.write(data, 0, count);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static File getAndMake(final File file) {
-		try {
-			if (!file.getParentFile().exists()) {
-				file.getParentFile().mkdirs();
-			}
-			if (!file.exists()) {
-				file.createNewFile();
-			}
+    @SuppressWarnings({"WeakerAccess", "ResultOfMethodCallIgnored"})
+    public static File getAndMake(final File file) {
+        try {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+            }
 
-		} catch (IOException ioException) {
-			System.err.println("Error while creating file '" + file.getName() + "'.");
-			System.err.println("Path: '" + file.getAbsolutePath() + "'");
-			ioException.printStackTrace();
-			throw new IllegalStateException();
-		}
-		return file;
-	}
+        } catch (IOException ioException) {
+            System.err.println("Error while creating file '" + file.getName() + "'.");
+            System.err.println("Path: '" + file.getAbsolutePath() + "'");
+            ioException.printStackTrace();
+            throw new IllegalStateException();
+        }
+        return file;
+    }
 
-	public static List<String> readAllLines(final File file) throws IOException {
-		final byte[] fileBytes = Files.readAllBytes(file.toPath());
-		final String asString = new String(fileBytes);
-		return new ArrayList<>(Arrays.asList(asString.split("\n")));
-	}
+    public static List<String> readAllLines(final File file) throws IOException {
+        final byte[] fileBytes = Files.readAllBytes(file.toPath());
+        final String asString = new String(fileBytes);
+        return new ArrayList<>(Arrays.asList(asString.split("\n")));
+    }
 }
