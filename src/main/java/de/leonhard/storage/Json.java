cd21a75d14b005cc649fc7b605b00a0fcaecaed1
EsGibtKeineMitte <Quaste17HD@t-online.de>
2019-10-16 16:43:50 +0200

    Moved FileData to FlatFile

--- src/main/java/de/leonhard/storage/Json.java
+++ src/main/java/de/leonhard/storage/Json.java
@@ -18,228 +18,227 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-@SuppressWarnings({"Duplicates", "unused", "WeakerAccess", "unchecked"})
 @Getter
 public class Json extends FlatFile implements StorageBase {
-    @Setter
-    private String pathPrefix;
-    private FileData fileData;
+	@Setter
+	private String pathPrefix;
 
 
-    public Json(final String name, final String path) {
-        try {
-            create(name, path, FileType.JSON);
-            if (getFile().length() == 0) {
+	public Json(final String name, final String path) {
+		try {
+			create(name, path, FileType.JSON);
+			if (getFile().length() == 0) {
 
-                Writer writer = new PrintWriter(new FileWriter(getFile().getAbsolutePath()));
-                writer.write(new JSONObject().toString(2));
-                writer.close();
-            }
+				Writer writer = new PrintWriter(new FileWriter(getFile().getAbsolutePath()));
+				writer.write(new JSONObject().toString(2));
+				writer.close();
+			}
 
-            update();
-        } catch (final Exception ex) {
-            System.err.println("Error creating JSON '" + file.getName() + "'");
-            System.err.println("In '" + file.getAbsolutePath() + "'");
-            ex.printStackTrace();
-        }
-    }
+			update();
+		} catch (final Exception ex) {
+			System.err.println("Error creating JSON '" + file.getName() + "'");
+			System.err.println("In '" + file.getAbsolutePath() + "'");
+			ex.printStackTrace();
+		}
+	}
 
-    public Json(final File file) {
-        this(file.getName().replace(".yml", ""), file.getAbsolutePath());
-    }
-
-
-    /**
-     * Sets a value to the json if the file doesn't already contain the value
-     * (Not mix up with Bukkit addDefault) Uses {@link JSONObject}
-     *
-     * @param key   Key to set the value
-     * @param value Value to set
-     */
-
-    @Override
-    public void setDefault(String key, Object value) {
-        if (contains(key)) {
-            return;
-        }
-        set(key, value);
-    }
-
-    private void reload() {
-        if (shouldReload()) {
-            update();
-        }
-    }
-
-    @Override
-    public Object get(final String key) {
-
-        String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
-
-        return getObject(finalKey);
-    }
-
-    private Object getObject(final String key) {
-        if (!has(key)) {
-            return null;
-        }
-
-        if (key.contains(".")) {
-            return new FileData(fileData.toMap()).containsKey(key) ? new FileData(fileData.toMap()).get(key) : null;
-        }
-        return fileData.containsKey(key) ? fileData.get(key) : null;
-    }
-
-    /**
-     * Gets a Map by key Although used to get nested objects {@link Json}
-     *
-     * @param key Path to Map-List in JSON
-     * @return Map
-     */
-
-    @Override
-    public Map getMap(final String key) {
-        String tempKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
-        if (!contains(tempKey)) {
-            return new HashMap();
-        } else {
-            return getMapWithoutPath(tempKey);
-        }
-    }
-
-    private Map getMapWithoutPath(final String key) {
-        reload();
-
-        if (!has(key)) {
-            return new HashMap<>();
-        }
-
-        Object map;
-        try {
-            map = getObject(key);
-        } catch (JSONException e) {
-            return new HashMap<>();
-        }
-        if (map instanceof Map) {
-            return (Map<?, ?>) fileData.get(key);
-        } else if (map instanceof JSONObject) {
-            return JsonUtils.jsonToMap((JSONObject) map);
-        }
-        throw new IllegalArgumentException("Json does not contain: '" + key + "'.");
-    }
-
-    @Override
-    public void set(final String key, final Object value) {
-        final String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
-
-        synchronized (this) {
-
-            reload();
-
-            FileData old = new FileData(fileData.toMap());
+	public Json(final File file) {
+		this(file.getName().replace(".yml", ""), file.getAbsolutePath());
+	}
 
 
-            fileData.insert(key, value);
+	/**
+	 * Sets a value to the json if the file doesn't already contain the value
+	 * (Not mix up with Bukkit addDefault) Uses {@link JSONObject}
+	 *
+	 * @param key   Key to set the value
+	 * @param value Value to set
+	 */
+
+	@Override
+	public void setDefault(String key, Object value) {
+		if (contains(key)) {
+			return;
+		}
+		set(key, value);
+	}
+
+	private void reload() {
+		if (shouldReload()) {
+			update();
+		}
+	}
+
+	@Override
+	public Object get(final String key) {
+
+		String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
+
+		return getObject(finalKey);
+	}
+
+	private Object getObject(final String key) {
+		if (!has(key)) {
+			return null;
+		}
+
+		if (key.contains(".")) {
+			return new FileData(fileData.toMap()).containsKey(key) ? new FileData(fileData.toMap()).get(key) : null;
+		}
+		return fileData.containsKey(key) ? fileData.get(key) : null;
+	}
+
+	/**
+	 * Gets a Map by key Although used to get nested objects {@link Json}
+	 *
+	 * @param key Path to Map-List in JSON
+	 * @return Map
+	 */
+
+	@Override
+	public Map getMap(final String key) {
+		String tempKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
+		if (!contains(tempKey)) {
+			return new HashMap();
+		} else {
+			return getMapWithoutPath(tempKey);
+		}
+	}
+
+	private Map getMapWithoutPath(final String key) {
+		reload();
+
+		if (!has(key)) {
+			return new HashMap<>();
+		}
+
+		Object map;
+		try {
+			map = getObject(key);
+		} catch (JSONException e) {
+			return new HashMap<>();
+		}
+		if (map instanceof Map) {
+			return (Map<?, ?>) fileData.get(key);
+		} else if (map instanceof JSONObject) {
+			return JsonUtils.jsonToMap((JSONObject) map);
+		}
+		throw new IllegalArgumentException("Json does not contain: '" + key + "'.");
+	}
+
+	@Override
+	public void set(final String key, final Object value) {
+		final String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
+
+		synchronized (this) {
+
+			reload();
+
+			FileData old = new FileData(fileData.toMap());
 
 
-            try {
-                if (old.toString().equals(fileData.toString()) && getFile().length() != 0) {
-                    return;
-                }
-                write(fileData.toJsonObject());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+			fileData.insert(key, value);
 
-        }
-    }
 
-    public void write(final JSONObject object) throws IOException {
-        Writer writer = new PrintWriter(new FileWriter(getFile().getAbsolutePath()));
-        writer.write(object.toString(3));
-        writer.close();
-    }
+			try {
+				if (old.toString().equals(fileData.toString()) && getFile().length() != 0) {
+					return;
+				}
+				write(fileData.toJsonObject());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-    @Override
-    public <T> T getOrSetDefault(final String path, T def) {
-        if (!contains(path)) {
-            set(path, def);
-            return def;
-        } else {
-            return (T) get(path);
-        }
-    }
+		}
+	}
 
-    @Override
-    public void update() {
-        final JSONTokener tokener = new JSONTokener(Objects.requireNonNull(FileUtils.createNewInputStream(file)));
-        fileData = new FileData(new JSONObject(tokener));
-    }
+	public void write(final JSONObject object) throws IOException {
+		Writer writer = new PrintWriter(new FileWriter(getFile().getAbsolutePath()));
+		writer.write(object.toString(3));
+		writer.close();
+	}
 
-    @Override
-    public Set<String> singleLayerKeySet() {
-        reload();
-        return new FileData(fileData.toMap()).singleLayerKeySet();
-    }
+	@Override
+	public <T> T getOrSetDefault(final String path, T def) {
+		if (!contains(path)) {
+			set(path, def);
+			return def;
+		} else {
+			return (T) get(path);
+		}
+	}
 
-    @Override
-    public Set<String> singleLayerKeySet(final String key) {
-        reload();
-        return new FileData(fileData.toMap()).singleLayerKeySet(key);
-    }
+	@Override
+	public void update() {
+		final JSONTokener tokener = new JSONTokener(Objects.requireNonNull(FileUtils.createNewInputStream(file)));
+		fileData = new FileData(new JSONObject(tokener));
+	}
 
-    @Override
-    public Set<String> keySet() {
-        reload();
-        return new FileData(fileData.toMap()).keySet();
-    }
+	@Override
+	public Set<String> singleLayerKeySet() {
+		reload();
+		return new FileData(fileData.toMap()).singleLayerKeySet();
+	}
 
-    @Override
-    public Set<String> keySet(final String key) {
-        reload();
-        return new FileData(fileData.toMap()).keySet(key);
-    }
+	@Override
+	public Set<String> singleLayerKeySet(final String key) {
+		reload();
+		return new FileData(fileData.toMap()).singleLayerKeySet(key);
+	}
 
-    private boolean has(final String key) {
-        reload();
-        return new FileData(fileData.toMap()).containsKey(key);
-    }
+	@Override
+	public Set<String> keySet() {
+		reload();
+		return new FileData(fileData.toMap()).keySet();
+	}
 
-    @Override
-    public boolean contains(final String key) {
-        String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
-        return has(finalKey);
-    }
+	@Override
+	public Set<String> keySet(final String key) {
+		reload();
+		return new FileData(fileData.toMap()).keySet(key);
+	}
 
-    @Override
-    public void remove(final String key) {
-        final String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
+	private boolean has(final String key) {
+		reload();
+		return new FileData(fileData.toMap()).containsKey(key);
+	}
 
-        final Map obj = fileData.toMap();
-        obj.remove(finalKey);
+	@Override
+	public boolean contains(final String key) {
+		String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
+		return has(finalKey);
+	}
 
-        fileData = new FileData(new JSONObject(obj));
-        try {
-            write(fileData.toJsonObject());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void remove(final String key) {
+		final String finalKey = (pathPrefix == null) ? key : pathPrefix + "." + key;
 
-    protected final Json getJsonInstance() {
-        return this;
-    }
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj == this) {
-            return true;
-        } else if (obj == null || this.getClass() != obj.getClass()) {
-            return false;
-        } else {
-            Json json = (Json) obj;
-            return this.fileData.equals(json.fileData)
-                    && this.pathPrefix.equals(json.pathPrefix)
-                    && super.equals(json.getFlatFileInstance());
-        }
-    }
+		final Map obj = fileData.toMap();
+		obj.remove(finalKey);
+
+		fileData = new FileData(new JSONObject(obj));
+		try {
+			write(fileData.toJsonObject());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected final Json getJsonInstance() {
+		return this;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == this) {
+			return true;
+		} else if (obj == null || this.getClass() != obj.getClass()) {
+			return false;
+		} else {
+			Json json = (Json) obj;
+			return this.fileData.equals(json.fileData)
+					&& this.pathPrefix.equals(json.pathPrefix)
+					&& super.equals(json.getFlatFileInstance());
+		}
+	}
 }
--- src/main/java/de/leonhard/storage/internal/base/FlatFile.java
+++ src/main/java/de/leonhard/storage/internal/base/FlatFile.java
@@ -8,100 +8,99 @@ import lombok.Setter;
 
 import java.io.File;
 
-@SuppressWarnings({"WeakerAccess", "UnusedReturnValue"})
 @Getter
 public abstract class FlatFile implements Comparable<FlatFile> {
-    @Setter
-    protected ReloadSettings reloadSettings = ReloadSettings.INTELLIGENT;
-    protected File file;
-    protected FileType fileType;
-    private long lastModified;
+	@Setter
+	protected ReloadSettings reloadSettings = ReloadSettings.INTELLIGENT;
+	protected File file;
+	protected FileType fileType;
+	protected FileData fileData;
+	private long lastModified;
 
-    /**
-     * Creates an empty .yml or .json file.
-     *
-     * @param name     Name of the file
-     * @param path     Absolute path where the file should be created
-     * @param fileType .yml/.json  Uses the Enum FileType
-     * @return true if file was created.
-     */
-    protected final synchronized boolean create(final String name, final String path, final FileType fileType) {
-        this.fileType = fileType;
-        this.file = new File(path, name + "." + fileType);
-        return generateFile(file);
-    }
+	/**
+	 * Creates an empty .yml or .json file.
+	 *
+	 * @param name     Name of the file
+	 * @param path     Absolute path where the file should be created
+	 * @param fileType .yml/.json  Uses the Enum FileType
+	 * @return true if file was created.
+	 */
+	protected final synchronized boolean create(final String name, final String path, final FileType fileType) {
+		this.fileType = fileType;
+		this.file = new File(path, name + "." + fileType);
+		return generateFile(file);
+	}
 
-    protected final synchronized boolean create(final File file) {
-        this.fileType = FileType.getFileType(file);
-        this.file = file;
-        return generateFile(file);
-    }
+	protected final synchronized boolean create(final File file) {
+		this.fileType = FileType.getFileType(file);
+		this.file = file;
+		return generateFile(file);
+	}
 
-    private synchronized boolean generateFile(File file) {
-        if (file.exists()) {
-            lastModified = System.currentTimeMillis();
-            return false;
-        } else {
-            FileUtils.getAndMake(file);
-            lastModified = System.currentTimeMillis();
-            return true;
-        }
-    }
+	private synchronized boolean generateFile(File file) {
+		if (file.exists()) {
+			lastModified = System.currentTimeMillis();
+			return false;
+		} else {
+			FileUtils.getAndMake(file);
+			lastModified = System.currentTimeMillis();
+			return true;
+		}
+	}
 
-    public final boolean shouldReload() {
-        if (reloadSettings.equals(ReloadSettings.AUTOMATICALLY)) {
-            return true;
-        } else if (reloadSettings.equals(ReloadSettings.INTELLIGENT)) {
-            return hasChanged();
-        } else {
-            return false;
-        }
-    }
+	public final boolean shouldReload() {
+		if (reloadSettings.equals(ReloadSettings.AUTOMATICALLY)) {
+			return true;
+		} else if (reloadSettings.equals(ReloadSettings.INTELLIGENT)) {
+			return hasChanged();
+		} else {
+			return false;
+		}
+	}
 
-    public boolean hasChanged() {
-        return FileUtils.hasChanged(file, lastModified);
-    }
+	public boolean hasChanged() {
+		return FileUtils.hasChanged(file, lastModified);
+	}
 
-    public final String getName() {
-        return this.file.getName();
-    }
+	public final String getName() {
+		return this.file.getName();
+	}
 
-    @SuppressWarnings("unused")
-    public final String getFilePath() {
-        return file.getAbsolutePath();
-    }
+	public final String getFilePath() {
+		return file.getAbsolutePath();
+	}
 
-    protected final FlatFile getFlatFileInstance() {
-        return this;
-    }
+	protected final FlatFile getFlatFileInstance() {
+		return this;
+	}
 
-    @Override
-    public synchronized boolean equals(final Object obj) {
-        if (obj == this) {
-            return true;
-        } else if (obj == null || this.getClass() != obj.getClass()) {
-            return false;
-        } else {
-            FlatFile flatFile = (FlatFile) obj;
-            return this.file.equals(flatFile.file)
-                    && this.lastModified == flatFile.lastModified
-                    && reloadSettings.equals(flatFile.reloadSettings)
-                    && fileType.equals(flatFile.fileType);
-        }
-    }
+	@Override
+	public synchronized boolean equals(final Object obj) {
+		if (obj == this) {
+			return true;
+		} else if (obj == null || this.getClass() != obj.getClass()) {
+			return false;
+		} else {
+			FlatFile flatFile = (FlatFile) obj;
+			return this.file.equals(flatFile.file)
+					&& this.lastModified == flatFile.lastModified
+					&& reloadSettings.equals(flatFile.reloadSettings)
+					&& fileType.equals(flatFile.fileType);
+		}
+	}
 
-    @Override
-    public synchronized int compareTo(final FlatFile flatFile) {
-        return this.file.compareTo(flatFile.file);
-    }
+	@Override
+	public synchronized int compareTo(final FlatFile flatFile) {
+		return this.file.compareTo(flatFile.file);
+	}
 
-    @Override
-    public synchronized int hashCode() {
-        return this.file.hashCode();
-    }
+	@Override
+	public synchronized int hashCode() {
+		return this.file.hashCode();
+	}
 
-    @Override
-    public synchronized String toString() {
-        return this.file.getAbsolutePath();
-    }
+	@Override
+	public synchronized String toString() {
+		return this.file.getAbsolutePath();
+	}
 }
