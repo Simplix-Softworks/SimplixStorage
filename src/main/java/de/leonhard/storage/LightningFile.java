b6f0fdf94a5331f5144d0d288ba30a09a9188ed2
Zeanon <thezeanon@gmail.com>
2019-10-16 17:03:35 +0200

    Added SupressWarnings

--- src/main/java/de/leonhard/storage/LightningFile.java
+++ src/main/java/de/leonhard/storage/LightningFile.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@SuppressWarnings("unused")
 @Getter
 public class LightningFile extends FlatFile implements StorageBase {
     @Setter
