dffff22b904d2811f35f9e73efaf0e9206921956
Leonhard <49833472+JavaFactoryDev@users.noreply.github.com>
2019-11-21 14:04:46 +0100

    Delete copy.sh

--- copy.sh
+++
@@ -1,7 +0,0 @@
-libFolder=/Users/leonhard//IdeaProjects/Libraries
-target=/Users/leonhard/Repository/
-
-cp $libFolder/LightningStorage/target/lightningstorage-3.0-Beta.jar $target
-cp $libFolder/protocolize/protocolize-plugin/target/protocolize-plugin.jar $target
-cp $libFolder/mccommons/mccommons-plugin/mccommons-plugin-proxy/target/mccommons-plugin-proxy-2.0-SNAPSHOT.jar $target
-cp $libFolder/mccommons/mccommons-plugin/mccommons-plugin-spigot/target/mccommons-plugin-spigot-2.0-SNAPSHOT.jar $target
