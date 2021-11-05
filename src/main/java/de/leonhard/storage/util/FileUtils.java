3f460b9a365871572d62d6fff8a87328558be863
ytcalifax <trayanovboris@gmail.com>
2021-11-05 22:16:02 +0200

    Last fix to make it build....

--- src/main/java/de/leonhard/storage/util/FileUtils.java
+++ src/main/java/de/leonhard/storage/util/FileUtils.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.StandardCopyOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
